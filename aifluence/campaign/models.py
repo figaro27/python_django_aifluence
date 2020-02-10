6a4bb7ced44d2dfebdf5254b1dbae2a543f62134
figaro27 <sweetfigaro27@gmail.com>
2020-02-10 02:37:14 +0100

    Change campaign table structure

--- aifluence/campaign/models.py
+++ aifluence/campaign/models.py
@@ -13,7 +13,7 @@ class Campaign(models.Model):
             models.CharField(max_length=255, blank=True),
         )
     )
-    brand_comptetitors = models.CharField(max_length=255)
+    brand_comptetitors = models.CharField(max_length=255, null=True, blank=True)
     brand_attributes = models.TextField(max_length=2000)
     key_selling_point = models.TextField(max_length=2000)
 
@@ -21,24 +21,32 @@ class Campaign(models.Model):
     age_range = ArrayField(
         ArrayField(
             models.CharField(max_length=50, blank=True),
-        )
+        ), 
+        null=True, 
+        blank=True
     )
     social_status = ArrayField(
         ArrayField(
             models.CharField(max_length=20, blank=True),
-        )
+        ), 
+        null=True, 
+        blank=True
     )
     interests = ArrayField(
         ArrayField(
             models.CharField(max_length=30, blank=True),
-        )
+        ), 
+        null=True, 
+        blank=True
     )
     personality = models.CharField(max_length=255)
     profession = models.CharField(max_length=255)
     location = ArrayField(
         ArrayField(
             models.CharField(max_length=30, blank=True),
-        )
+        ), 
+        null=True, 
+        blank=True
     )
     touchpoint = models.CharField(max_length=255, null=True, blank=True)
 
@@ -47,10 +55,10 @@ class Campaign(models.Model):
     campaign_budget = models.IntegerField()
     campaign_currency = models.CharField(max_length=3, choices=CONSTANTS.CURRENCY_CHOICES, default="usd")
     campaign_kpis = models.TextField(max_length=2000)
-    active_social_media = models.CharField(max_length=64)
-    previous_marketing_campaigns = models.CharField(max_length=64)
-    owned_event = models.CharField(max_length=64)
-    sponsored_event = models.CharField(max_length=64)
+    active_social_media = models.CharField(max_length=64, null=True, blank=True)
+    previous_marketing_campaigns = models.CharField(max_length=64, null=True, blank=True)
+    owned_event = models.CharField(max_length=64, null=True, blank=True)
+    sponsored_event = models.CharField(max_length=64, null=True, blank=True)
     client = models.ForeignKey(User, on_delete=CASCADE, null=True, related_name='client')
     agent = models.ForeignKey(User, on_delete=CASCADE, null=True, related_name='agent')
     created_at = models.DateTimeField(auto_now_add=True)
